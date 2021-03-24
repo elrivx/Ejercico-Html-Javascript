125ab72164ce7db2791c05d165eea3d3a0096f6a
Rodrigo Rivillo Brito <elrivx@pop-os.localdomain>
2021-03-24 17:57:35 -0300

    Así se realiza un ejercico básico entre HTML y Javascript.

---
+++ Ejercicio/index.js
@@ -0,0 +1,22 @@
+var num = 15;
+var name = "Bruce";
+
+var num1 = 10;
+var num2 = 15;
+
+if (num >=10){
+  console.log("El número es mayor que 10");
+}
+
+if (name != "Bruce"){
+  console.log("¿Cuál es tu nombre?");
+}
+else{
+  console.log("Hola Bruce!");
+}
+
+if (num1 <= num2){
+  num1 = num1 + 10;
+}
+console.log(num1);
+console.log(num2);
